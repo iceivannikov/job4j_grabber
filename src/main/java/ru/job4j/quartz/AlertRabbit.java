63d0729f646d6b367ac68bb249a60c60cc4ae0d7
Viktor Ivannikov <iceivannikov@gmail.com>
2024-11-01 00:53:32 +0300

    1.1. Job c параметрами [#260360]

--- src/main/java/ru/job4j/quartz/AlertRabbit.java
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -3,9 +3,9 @@ package ru.job4j.quartz;
 import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.sql.*;
 import java.util.Properties;
 
 import static org.quartz.JobBuilder.*;
@@ -17,7 +17,11 @@ public class AlertRabbit {
         try {
             Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
             scheduler.start();
-            JobDetail job = newJob(Rabbit.class).build();
+            JobDataMap data = new JobDataMap();
+            data.put("dbProperties", loadProperties());
+            JobDetail job = newJob(Rabbit.class)
+                    .usingJobData(data)
+                    .build();
             SimpleScheduleBuilder times = simpleSchedule()
                     .withIntervalInSeconds(getInterval())
                     .repeatForever();
@@ -26,7 +30,9 @@ public class AlertRabbit {
                     .withSchedule(times)
                     .build();
             scheduler.scheduleJob(job, trigger);
-        } catch (SchedulerException se) {
+            Thread.sleep(10000);
+            scheduler.shutdown();
+        } catch (SchedulerException | InterruptedException se) {
             throw new RuntimeException(se);
         }
     }
@@ -35,16 +41,34 @@ public class AlertRabbit {
         @Override
         public void execute(JobExecutionContext context) {
             System.out.println("Rabbit runs here ...");
+            Properties properties = (Properties) context.getJobDetail().getJobDataMap().get("dbProperties");
+            try (Connection connection = DriverManager.getConnection(
+                    properties.getProperty("url"),
+                    properties.getProperty("username"),
+                    properties.getProperty("password"));
+                 PreparedStatement ps = connection.prepareStatement(
+                         "INSERT INTO rabbit (created_date) VALUES (?)")) {
+                ps.setTimestamp(1, new Timestamp(System.currentTimeMillis()));
+                ps.executeUpdate();
+            } catch (SQLException e) {
+                throw new RuntimeException("Error connecting to database", e);
+            }
         }
     }
 
     private static int getInterval() {
+        Properties properties = loadProperties();
+        return Integer.parseInt(properties.getProperty("rabbit.interval"));
+    }
+
+    private static Properties loadProperties() {
         Properties properties = new Properties();
-        try (InputStream is = new FileInputStream("src/main/resources/rabbit.properties")) {
+        try (InputStream is = AlertRabbit.class.getClassLoader()
+                .getResourceAsStream("rabbit.properties")) {
             properties.load(is);
-            return Integer.parseInt(properties.getProperty("rabbit.interval"));
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Error loading properties file", e);
         }
+        return properties;
     }
 }
